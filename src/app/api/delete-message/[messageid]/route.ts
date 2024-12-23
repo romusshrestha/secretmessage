32120ae0907d1fc73095f998b4570e788bd69483
Romus Shrestha <sromus498@gmail.com>
2024-12-23 13:44:28 +0545

    fix

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,67 +1,63 @@
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/User";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
 import { User } from "next-auth";
 
-export async function DELETE(request: Request, { params }: { params: { messageid: string } }) {
-    const messageId = params.messageid;
-    await dbConnect();
-    const session = await getServerSession(authOptions);
-    const user: User = session?.user as User;
-    if (!session || !session.user) {
-        return Response.json(
-            {
-                success: false,
-                message: "Not Authenticated",
-            },
-            {
-                status: 402
-            }
-        )
-    }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageid: string } } // Type for the params object
+) {
+  const messageId = params.messageid;
+  await dbConnect();
 
-    try {
-        const updatedResult = await UserModel.updateOne(
-            {
-                _id: user._id
-            },
-            {
-                $pull: {
-                    messages: { _id: messageId }
-                }
-            }
-        )
-        if (updatedResult.matchedCount===0) {
-            return Response.json(
-                {
-                    success: false,
-                    message: "Message not found or already deleted",
-                },
-                {
-                    status: 404
-                }
-            )
-        }
-        return Response.json(
-            {
-                success: true,
-                message: "Message deleted",
-            },
-            {
-                status: 200
-            }
-        )
-    } catch (error) {
-        console.log("Error deleting message route: ", error);
-        return Response.json(
-            {
-                success: false,
-                message: "Error deleteing message",
-            },
-            {
-                status: 500
-            }
-        )
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  //check session and user available
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted",
+        },
+        { status: 404 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: "Message  deleted",
+        },
+        { status: 200 }
+      );
     }
+  } catch (error) {
+    console.log("Error deleting message:", error);
+
+    return Response.json(
+      {
+        success: false,
+        message: "Error deleting message",
+      },
+      { status: 500 }
+    );
+  }
 }
