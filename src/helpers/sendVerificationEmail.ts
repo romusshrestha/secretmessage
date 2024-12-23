fee9a02e5938f2b28e43e79b48580c63ebbe4735
Romus Shrestha <sromus498@gmail.com>
2024-12-23 14:53:38 +0545

    fix 6

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,12 +1,12 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { IApiResponse } from "@/types/ApiResponse";
+import  ApiResponse  from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(
     email: string,
     username: string,
     verifyCode: string,
-): Promise<IApiResponse> {
+): Promise<ApiResponse> {
     try {
         await resend.emails.send({
             from: 'onboarding@resend.dev',
